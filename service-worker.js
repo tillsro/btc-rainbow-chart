a39f4c473e23d913a2f14c44e1c2385ae0df5663
fractal100 <tillsro@gmail.com>
2026-06-25 21:51:03 -0400

    Bump PWA cache for granularity control

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "btc-rainbow-pwa-v3";
+const CACHE_NAME = "btc-rainbow-pwa-v4";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./assets/icon.svg"];
 
 self.addEventListener("install", event => {
