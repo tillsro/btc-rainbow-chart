55fb0ab2d0e93d7283cb75db9ea19f133e6826d0
fractal100 <tillsro@gmail.com>
2026-06-25 22:35:05 -0400

    Bump cache for x-axis label fix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "btc-rainbow-pwa-v10";
+const CACHE_NAME = "btc-rainbow-pwa-v11";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./assets/icon.svg"];
 
 self.addEventListener("install", event => {
